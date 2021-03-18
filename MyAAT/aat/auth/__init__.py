6400d4243a6713c5f38bee2d7a754307f8312337
ioanclarke <ioanclarke@gmail.com>
2021-03-18 16:18:58 +0000

    added static folder for each blueprint

--- MyAAT/aat/auth/__init__.py
+++ MyAAT/aat/auth/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-bp = Blueprint('auth', __name__, template_folder='templates')
+bp = Blueprint('auth', __name__, template_folder='templates', static_folder='static')
 
 from aat.auth import routes
--- MyAAT/aat/errors/__init__.py
+++ MyAAT/aat/errors/__init__.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-bp = Blueprint('errors', __name__, template_folder='templates')
+bp = Blueprint('errors', __name__, template_folder='templates', static_folder='static')
 
 from aat.errors import routes
