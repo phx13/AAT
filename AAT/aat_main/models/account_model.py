f63022e10ebd964304e9ebb854ba977a469db241
phx <945871257@qq.com>
2021-03-18 20:15:07 +0800

    change mysql password

--- AAT/aat_main/models/account_model.py
+++ AAT/aat_main/models/account_model.py
@@ -1,4 +1,5 @@
 from sqlalchemy import MetaData, Table
+from sqlalchemy.exc import SQLAlchemyError
 
 from aat_main import db
 from aat_main.utils.api_exception_helper import InterServerErrorException
@@ -8,29 +9,31 @@ class AccountModel(db.Model):
     __tablename__ = 'account'
     __table__ = Table(__tablename__, MetaData(bind=db.engine), autoload=True)
 
-    def search_account_by_id(self, id):
+    @staticmethod
+    def search_account_by_id(id):
         try:
             return db.session.query(AccountModel).filter_by(id=id).first()
-        except:
+        except SQLAlchemyError:
             return InterServerErrorException()
 
-    def create_account(self, id, email, password, name):
+    @staticmethod
+    def create_account(id, email, password, name):
         try:
             db.session.add(AccountModel(id=id, email=email, password=password, name=name))
             db.session.commit()
-        except:
+        except SQLAlchemyError:
             return InterServerErrorException()
 
     def update_account(self, id, email, password, name):
         try:
             self.search_account_by_id(id).update({'email': email, 'password': password, 'name': name})
             db.session.commit()
-        except:
+        except SQLAlchemyError:
             return InterServerErrorException()
 
     def delete_account(self, id):
         try:
             self.search_account_by_id(id).delete()
             db.session.commit()
-        except:
+        except SQLAlchemyError:
             return InterServerErrorException()
--- AAT/aat_main/utils/base64_helper.py
+++ AAT/aat_main/utils/base64_helper.py
@@ -3,7 +3,8 @@ import os
 
 
 class Base64Helper:
-    def picture_to_base64(self, image_name):
+    @staticmethod
+    def picture_to_base64(image_name):
         try:
             with open('aat_main/resources/images/' + image_name, 'rb') as f:
                 base64_code = base64.b64encode(f.read()).decode()
@@ -11,7 +12,8 @@ class Base64Helper:
         except:
             return Exception
 
-    def base64_to_picture(self, base64_data, image_name):
+    @staticmethod
+    def base64_to_picture(base64_data, image_name):
         if base64_data.startswith("data:image/"):
             is_existed = os.path.exists('aat_main/resources/images/base64')
             if not is_existed:
