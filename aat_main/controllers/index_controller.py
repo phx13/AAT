eeb6090a87388896dde689bdcecdfaf7711f7128
phx <945871257@qq.com>
2021-03-22 23:07:51 +0800

    add echarts example

--- aat_main/controllers/index_controller.py
+++ aat_main/controllers/index_controller.py
@@ -7,3 +7,8 @@ index_bp = Blueprint('index_bp', __name__, static_folder='resources')
 @index_bp.route('/')
 def home():
     return render_template('index.html')
+
+
+@index_bp.route('/echarts/')
+def echarts():
+    return render_template('echarts.html')
