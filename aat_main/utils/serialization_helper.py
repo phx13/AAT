d4a8fba165f408aab2c84368362d390d408cface
phx <945871257@qq.com>
2021-03-23 23:14:14 +0800

    Rename db_helper.py to serialization_helper.py

--- aat_main/utils/serialization_helper.py
+++ aat_main/utils/serialization_helper.py
@@ -1,4 +1,11 @@
+from typing import List
+
+
 class SerializationHelper:
+    # reference. 23 march https://stackoverflow.com/questions/68645/are-static-class-variables-possible-in-python
+    ENCODE_PAIR = ',,,'
+    ENCODE_NEXT_ITEM = '&&&'
+
     @staticmethod
     def model_to_list(models):
         list_data = []
@@ -25,3 +32,46 @@ class SerializationHelper:
                         dict_data[k2] = v2
             list_data.append(dict_data)
         return list_data
+
+    @staticmethod
+    def encode(s1: tuple, s2: tuple) -> str:
+        return f'{s1[0]}{SerializationHelper.ENCODE_PAIR}{s1[1]}{SerializationHelper.ENCODE_NEXT_ITEM}{s2[0]}{SerializationHelper.ENCODE_PAIR}{s2[1]}'
+
+    @staticmethod
+    def decode(sr_raws: List[str], responses: dict) -> List[dict]:
+        """
+        This produces a list of the form
+        [
+            {
+                'statement': statement1
+                'responses': {
+                    'Strongly disagree': x
+                    ...
+                    'Strongly agree': y
+            },
+            {
+                'statement': statement2
+                'responses': {
+                    'Strongly disagree': w
+                    ...
+                    'Strongly agree': z
+            }
+        ]
+        """
+        statement_counts = []
+        for sr_map in sr_raws:
+            qas = sr_map.split(SerializationHelper.ENCODE_NEXT_ITEM)
+            for qa in qas:
+                q, a = qa.split(SerializationHelper.ENCODE_PAIR)
+                # print([s['statement'] for s in statement_counts])
+                if not any(s['statement'] == q for s in statement_counts):
+                    # print(f'{q} not in {statement_counts}\n')
+                    statement_counts.append({
+                        'statement': q,
+                        'responses': {response: 0 for response in responses.values()}
+                    })
+                for sc in statement_counts:
+                    if sc['statement'] == q:
+                        response = responses[int(a)]
+                        sc['responses'][response] = sc['responses'][response] + 1
+        return statement_counts
