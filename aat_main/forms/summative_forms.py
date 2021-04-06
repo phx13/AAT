d06039782eb115e9e2ab13725313b0411d4745ca
Matthew Howells <c2106366@cardiff.ac.uk>
2021-04-06 11:26:52 +0100

    Temporary create question paage + form)

--- aat_main/forms/summative_forms.py
+++ aat_main/forms/summative_forms.py
@@ -4,5 +4,6 @@ from wtforms import SubmitField, TextAreaField
 
 class assessment_form(FlaskForm):
     title = TextAreaField('Assessment Title')
+    questions = TextAreaField('Choose some questions')
     # description = TextAreaField('Assessment Description')
     submit = SubmitField('Submit')
