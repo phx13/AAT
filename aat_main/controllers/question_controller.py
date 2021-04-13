61ca68fcf68d5bb07d4c4d8f901b1354934aa6f4
Yaoyutong Wu <wuy120@cardiff.ac.uk>
2021-04-13 21:02:43 +0800

    add question management logic

--- aat_main/controllers/question_controller.py
+++ aat_main/controllers/question_controller.py
@@ -1,69 +1,120 @@
-from flask import Blueprint, render_template, flash, request, redirect, url_for
+from flask import Blueprint, render_template, flash, request, redirect, url_for, jsonify
 from jinja2 import TemplateError
+from sqlalchemy.exc import SQLAlchemyError
 
-from aat_main.utils.api_exception_helper import NotFoundException
+from aat_main.models.question_models import QuestionData
+from aat_main.utils.api_exception_helper import NotFoundException, InterServerErrorException
 
-question_blueprint = Blueprint('question_blueprint', __name__)
+question_bp = Blueprint('question_bp', __name__, template_folder='../views/question')
 
 
-@question_blueprint.route('/course/assessment/<int:assessment_id>')
+@question_bp.route('/question/management/')
+def question_page():
+    try:
+        return render_template('question_management.html')
+    except TemplateError:
+        raise NotFoundException()
+
+
+@question_bp.route('/question/management/data/', methods=['GET'])
+def question_data():
+    try:
+        origin_data = QuestionData.search_all()
+        data = []
+        for od in origin_data:
+            dic = {}
+            dic['id'] = od.id
+            dic['course'] = od.course
+            dic['question'] = od.question
+            dic['option'] = od.option
+            dic['answer'] = od.answer
+            dic['release_time'] = od.release_time
+            data.append(dic)
+        if request.method == 'GET':
+            info = request.values
+            limit = info.get('limit', 10)
+            offset = info.get('offset', 0)
+        return jsonify({'total': len(data), 'rows': data[int(offset):(int(offset) + int(limit))]})
+    except TemplateError:
+        raise NotFoundException()
+
+
+@question_bp.route('/question/management/data/', methods=['POST'])
+def delete_question_data():
+    try:
+        for k,v in request.form.items():
+            QuestionData.delete_question_by_id(k)
+        return 'delete successful'
+    except:
+        return 'Server error'
+
+
+@question_bp.route('/course/assessment/<int:assessment_id>')
 def assessment_page():
     try:
         return render_template('assessment.html')
     except TemplateError:
         raise NotFoundException()
 
-@question_blueprint.route('/course/assessment/<int:assessment_id>/multiplechoice<question_id>')
+
+@question_bp.route('/course/assessment/<int:assessment_id>/multiplechoice<question_id>')
 def type_one_question_page():
     try:
         return render_template('type_one_question.html')
     except TemplateError:
         raise NotFoundException()
 
-@question_blueprint.route('/course/assessment/<int:assessment_id>/fillinblank<question_id>')
+
+@question_bp.route('/course/assessment/<int:assessment_id>/fillinblank<question_id>')
 def type_two_question_page():
     try:
         return render_template('type_two_question.html')
     except TemplateError:
         raise NotFoundException()
 
-@question_blueprint.route('/course/assessment/<int:assessment_id>/multiplechoice<question_id>/edit')
+
+@question_bp.route('/course/assessment/<int:assessment_id>/multiplechoice<question_id>/edit')
 def edit_type_one_question_edit_page():
     try:
         return render_template('type_one_question_edit.html')
     except TemplateError:
         raise NotFoundException()
 
-@question_blueprint.route('/course/assessment/<int:assessment_id>/fillinblank<question_id>/edit')
+
+@question_bp.route('/course/assessment/<int:assessment_id>/fillinblank<question_id>/edit')
 def edit_type_two_question_edit_page():
     try:
         return render_template('type_two_question_edit.html')
     except TemplateError:
         raise NotFoundException()
 
-@question_blueprint.route('/course/assessment/<int:assessment_id>/multiplechoice<question_id>/save')
+
+@question_bp.route('/course/assessment/<int:assessment_id>/multiplechoice<question_id>/save')
 def edit_type_one_question():
     try:
         return render_template('type_one_question.html')
     except TemplateError:
         raise NotFoundException()
 
-@question_blueprint.route('/course/assessment/<int:assessment_id>/fillinblank<question_id>/save')
+
+@question_bp.route('/course/assessment/<int:assessment_id>/fillinblank<question_id>/save')
 def save_type_two_question():
     try:
         return render_template('type_two_question.html')
     except TemplateError:
         raise NotFoundException()
 
-@question_blueprint.route('/course/assessment/<int:assessment_id>/multiplechoice<question_id>/remove')
+
+@question_bp.route('/course/assessment/<int:assessment_id>/multiplechoice<question_id>/remove')
 def edit_type_one_question_remove():
     try:
         return redirect(url_for('assessment_page'))
     except TemplateError:
         raise NotFoundException()
 
-@question_blueprint.route('/course/assessment/<int:assessment_id>/multiplechoice<question_id>/remove')
-def edit_type_one_question_remove():
+
+@question_bp.route('/course/assessment/<int:assessment_id>/multiplechoice<question_id>/remove')
+def edit_type_two_question_remove():
     try:
         return redirect(url_for('assessment_page'))
     except TemplateError:
--- aat_main/models/question_models.py
+++ aat_main/models/question_models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import MetaData, Table
+from sqlalchemy.exc import SQLAlchemyError
 
 from aat_main import db
 
@@ -11,6 +12,38 @@ class Question(db.Model):
     description: varchar(256)
     course: varchar(64)
     """
+
+    @staticmethod
+    def get_question_by_id(id):
+        return db.session.query(Question).get(id)
+
+    def create_question(name, description, course):
+        db.session.add(Question(name=name, description=description, course=course))
+        db.session.commit()
+
+
+class QuestionData(db.Model):
+    __tablename__ = 'questiondata'
+    __table__ = Table(__tablename__, MetaData(bind=db.engine), autoload=True)
+    """
+    id: int, auto_increment, primary
+    name: varchar(128)
+    description: varchar(256)
+    course: varchar(64)
+    """
+
+
+    @staticmethod
+    def search_all():
+        return db.session.query(QuestionData).all()
+
+    @staticmethod
+    def delete_question_by_id(id):
+        try:
+            db.session.query(QuestionData).filter_by(id=id).delete()
+            db.session.commit()
+        except SQLAlchemyError:
+            return 'Server error'
 
     @staticmethod
     def get_question_by_id(id):
