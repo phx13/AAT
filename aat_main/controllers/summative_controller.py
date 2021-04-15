bd618b9feeff9d0c452d53f54a4a3cab3de693bc
Matthew Howells <c2106366@cardiff.ac.uk>
2021-04-15 01:12:33 +0100

    Form validation for module aded. Bootstrap styling added.

--- aat_main/controllers/summative_controller.py
+++ aat_main/controllers/summative_controller.py
@@ -1,5 +1,5 @@
 import json
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from jinja2 import TemplateError
 
 from aat_main.forms.summative_forms import assessment_form
@@ -14,24 +14,26 @@ summative_blueprint = Blueprint('summative_blueprint', __name__, template_folder
 def summative():
     questions = Question.get_all()
     form = assessment_form()
-    if form.validate_on_submit():
-        added_questions = []
-        # Checks if checkbox is active. If yes, adds value to added questions.
-        for question in questions:
-            question_id = request.form.get(str(question.id))
-            if question_id:
-                added_questions.append(question_id)
+    if request.method == "POST":
+        if form.validate_on_submit():
+            added_questions = []
+            # Checks if checkbox is active. If yes, adds value to added questions.
+            for question in questions:
+                question_id = request.form.get(str(question.id))
+                if question_id:
+                    added_questions.append(question_id)
 
-        #Gets module code
-        module_code = form.module.data[:6]
+            #Gets module code
+            module_code = form.module.data[:6]
 
-        #Convert Datetime's
-        start_datetime = Assessment.convert_datetime(form.start_date.data, form.start_time.data)
-        end_datetime = Assessment.convert_datetime(form.end_date.data, form.end_time.data)
-    
-        added_questions = json.dumps(added_questions)
-        Assessment.create_assessment(form.title.data, added_questions, form.description.data, module_code, start_datetime, end_datetime)
-        return redirect(url_for('course_bp.assessments'))
+            #Convert Datetime's
+            start_datetime = Assessment.convert_datetime(form.start_date.data, form.start_time.data)
+            end_datetime = Assessment.convert_datetime(form.end_date.data, form.end_time.data)
+        
+            added_questions = json.dumps(added_questions)
+            Assessment.create_assessment(form.title.data, added_questions, form.description.data, module_code, start_datetime, end_datetime)
+            return redirect(url_for('course_bp.assessments'))
+
 
     return render_template("summative.html", form=form, questions=questions)
     # try:
--- aat_main/forms/summative_forms.py
+++ aat_main/forms/summative_forms.py
@@ -1,25 +1,33 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, TextAreaField, BooleanField, SelectField
 from wtforms.fields.html5 import DateField, TimeField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 from aat_main.models.module_model import Module
-
 
 
 
 class assessment_form(FlaskForm):
     MODULE_CHOICES = {}
+    MODULE_CHOICES["Please Choose a Module"] = 0
     modules = Module.get_all()
     for module in modules:
-        MODULE_CHOICES[module.code + "  :  " +module.name] = module.code 
+        MODULE_CHOICES[module.code + "  :  " + module.name] = module.code 
+
+    def validate_module(form, module):
+        if module.data == "Please Choose a Module":
+            raise ValidationError("Please Choose a Module!")
 
     title = TextAreaField('Assessment Title', validators=[DataRequired()])
     # Course will be changed to only show courses that each lecturer is on. 
-    module = SelectField('Modules: ', choices=MODULE_CHOICES)
+    module = SelectField(choices=MODULE_CHOICES)
     description = TextAreaField('Assessment Description', validators=[DataRequired()])
     start_date = DateField("Start Date", format='%Y-%m-%d', validators=[DataRequired()])
     start_time = TimeField("Start Time", validators=[DataRequired()])
     end_date = DateField("End Date", format='%Y-%m-%d', validators=[DataRequired()])
     end_time = TimeField("End Time", validators=[DataRequired()])
     submit = SubmitField()
+
+
+
+    
 
