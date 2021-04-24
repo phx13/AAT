230d18304a17fae43e3a1f70dbe246f57243206e
Matthew Howells <c2106366@cardiff.ac.uk>
2021-04-24 16:40:45 +0100

    Summative form functionality done. Requires validation

--- aat_main/controllers/summative_controller.py
+++ aat_main/controllers/summative_controller.py
@@ -17,8 +17,13 @@ from aat_main import db
 
 summative_blueprint = Blueprint('summative_blueprint', __name__, template_folder='../views/summative')
 
-@summative_blueprint.route('/assessments/assessments_management/summative/', methods=['GET', 'POST'])
+@summative_blueprint.route('/assessments/assessment_management/summative')
 def summative():
+    assessments = Assessment.get_all()
+    return render_template("summative.html", assessments=assessments)
+
+@summative_blueprint.route('/assessments/assessment_management/summative/create', methods=['GET', 'POST'])
+def summative_create():
     questions = Question.get_all()
     form = assessment_form()
     module_db = Module.get_all()
@@ -29,6 +34,22 @@ def summative():
         ).filter(ModuleEnrolment.account_id == current_user.id).all()]
     module_choices.insert(0,("Please Choose a Module", "Please Choose a Module"))
     form.module.choices = module_choices
+
+    # To validate modules lecturer is enroled on and check questions + gets question ids (CLEAN THIS UP)
+    valid_modules = [(mod.code
+        ) for mod in db.session.query(
+        Module).join(ModuleEnrolment,Module.code == ModuleEnrolment.module_code
+        ).filter(ModuleEnrolment.account_id == current_user.id).all()]
+
+    valid_questions =[]
+    question_id = []
+    for question in questions:
+        if question.module_code in valid_modules:
+            question_id.append(question.id)
+            valid_questions.append(question)
+    
+    question_id = json.dumps(question_id)
+
 
     if request.method == "POST":
         if form.validate_on_submit():
@@ -54,13 +75,13 @@ def summative():
                                          start_datetime, end_datetime, form.timelimit.data, datetime.now())
             return redirect(url_for('assessment_bp.assessments'))
 
-    return render_template("summative.html", form=form, questions=questions, modules=module_choices)
+    return render_template("summative_create.html", form=form, questions=valid_questions, modules=module_choices, question_id=question_id, valid_modules=valid_modules)
     # try:
     #     return render_template('summative.html')
     # except TemplateError:
     #     raise NotFoundException()
 
-@summative_blueprint.route('/assessments/assessment_management/<int:assessment_id>', methods=['GET', 'POST'])
+@summative_blueprint.route('/assessments/assessment_management/summative/<int:assessment_id>', methods=['GET', 'POST'])
 def summative_edit(assessment_id):
     assessment = Assessment.query.get_or_404(assessment_id)
     form = summative_edit_form()
@@ -106,6 +127,17 @@ def summative_edit(assessment_id):
     return render_template("summative_edit.html", assessment=assessment, form=form, 
     questions=questions, startdate=startdate, starttime=starttime, 
     enddate=enddate, endtime=endtime, added_questions=added_questions)
+
+
+@summative_blueprint.route('/delete/<int:assessment_id>/<action>')
+def summative_delete_action(assessment_id, action):
+    assessment = Assessment.query.filter_by(id=assessment_id).first_or_404()
+
+    if action == 'delete':
+        assessment.delete_assessment(assessment_id)
+        db.session.commit()
+    
+    return redirect(request.referrer)
 
 @summative_blueprint.route('/course/assessment/')
 def course_assessment_page():
--- aat_main/models/assessment_models.py
+++ aat_main/models/assessment_models.py
@@ -58,15 +58,26 @@ class Assessment(db.Model):
 
     @staticmethod
     def update_assessment(title, questions, description, start_datetime, end_datetime, timelimit, assessment_id):
-        assessment = Assessment.query.filter_by(id=assessment_id).first()
-        assessment.title = title
-        assessment.questions = questions
-        assessment.description = description
-        assessment.availability_date = start_datetime
-        assessment.due_date = end_datetime
-        assessment.timelimit = timelimit
-        # (title=title, questions=questions, description=description, availability_date=start_datetime, due_date=end_datetime, timelimit=timelimit)
-        db.session.commit()
+        try:
+            assessment = Assessment.query.filter_by(id=assessment_id).first()
+            assessment.title = title
+            assessment.questions = questions
+            assessment.description = description
+            assessment.availability_date = start_datetime
+            assessment.due_date = end_datetime
+            assessment.timelimit = timelimit
+            db.session.commit()
+        except SQLAlchemyError:
+            raise SQLAlchemyError
+
+    @staticmethod
+    def delete_assessment(assessment_id):
+        try:
+            assessment = Assessment.query.filter_by(id=assessment_id).first()
+            db.session.delete(assessment)
+            db.session.commit()
+        except SQLAlchemyError:
+            raise SQLAlchemyError
 
     @staticmethod
     def get_all_current(time):
