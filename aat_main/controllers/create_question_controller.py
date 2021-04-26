9516ac17b7fdb9e2891977418138a6cb5a5935bf
ioanclarke <ioanclarke@gmail.com>
2021-04-26 17:54:21 +0100

    merging before pull

--- aat_main/controllers/create_question_controller.py
+++ aat_main/controllers/create_question_controller.py
@@ -3,7 +3,7 @@ from flask_login import current_user
 from jinja2 import TemplateError
 
 from aat_main import db
-from aat_main.forms.question_form import question_form
+# from aat_main.forms.question_form import question_form
 from aat_main.models.question_models import Question
 from aat_main.utils.api_exception_helper import NotFoundException
 
@@ -12,12 +12,13 @@ create_question_blueprint = Blueprint('create_question_blueprint', __name__, tem
 
 @create_question_blueprint.route('/', methods=['GET', 'POST'])
 def create_question():
-    form = question_form()
-    if form.validate_on_submit():
-        Question.create_question(form.name.data, form.description.data, form.course.data)
-        return redirect(url_for('assessment_bp.assessments'))
-
-    return render_template("create_question.html", form=form)
+    pass
+    # form = question_form()
+    # if form.validate_on_submit():
+    #     Question.create_question(form.name.data, form.description.data, form.course.data)
+    #     return redirect(url_for('assessment_bp.assessments'))
+    #
+    # return render_template("create_question.html", form=form)
     # try:
     #     return render_template('question.html')
     # except TemplateError:
