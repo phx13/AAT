b860dc94f701bd4cd67970188af59dd1a06317be
Zhongwang <lzw123@cardiff.ac.uk>
2021-05-02 18:25:03 +0100

    formative assessment create and edit

--- aat_main/forms/formative_forms.py
+++ aat_main/forms/formative_forms.py
@@ -1,3 +1,4 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, TextAreaField, BooleanField, SelectField, IntegerField
 from wtforms.fields.html5 import DateField, TimeField
@@ -6,19 +7,20 @@ from aat_main.models.module_model import Module
 
 
 class module_choice_form(FlaskForm):
-    MODULE_CHOICES = {}
-    MODULE_CHOICES["Please Choose a Module"] = 0
-    #belongs to current account
-    modules = Module.get_all()
-    for module in modules:
-        MODULE_CHOICES[module.code + "  :  " + module.name] = module.code
-
-    def validate_module(form, module):
-        if module.data == "Please Choose a Module":
-            raise ValidationError("Please Choose a Module!")
-
-    module = SelectField(choices=MODULE_CHOICES)
-    submit = SubmitField()
+    pass
+    # MODULE_CHOICES = {}
+    # MODULE_CHOICES["Please Choose a Module"] = 0
+    # #belongs to current account
+    # modules = current_user.get_enrolled_modules()
+    # for module in modules:
+    #     MODULE_CHOICES[module.code + "  :  " + module.name] = module.code
+    #
+    # def validate_module(form, module):
+    #     if module.data == "Please Choose a Module":
+    #         raise ValidationError("Please Choose a Module!")
+    #
+    # module = SelectField(choices=MODULE_CHOICES)
+    # submit = SubmitField()
 
 
 
